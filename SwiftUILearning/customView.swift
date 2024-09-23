dfc8feec3c89780fc4ae69a9e62d7c819985db7b
jinpengfei <jinpengfei027873@gtjas.com>
2024-09-23 16:47:29 +0800

    更新

---
+++ SwiftUILearning/customView.swift
@@ -0,0 +1,18 @@
+//
+//  customView.swift
+//  SwiftUILearning
+//
+//  Created by 金鹏飞 on 2024/9/23.
+//
+
+import SwiftUI
+
+struct customView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    customView()
+}
